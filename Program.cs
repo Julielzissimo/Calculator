5d2d28bc1443388aa81488b229e710c773ac1ffa
Juliel Mendonça <juliel.mendonca@gmail.com>
2023-07-10 15:04:08 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine()); //Readline sempre retorna uma string. Caso queira um float, use Parse.
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da soma é {v1 + v2}"); // Ao usar o cifrão, denomina-se interpolação de strings.
+        }
+    }
+}
